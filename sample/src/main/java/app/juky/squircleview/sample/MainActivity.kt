9cf122a788f4070ad596ed67ad1881c1127385f4
Niels G <Nielssg@users.noreply.github.com>
2021-06-12 11:10:37 +0200

    Added support for Android Shapes
    Closes #2

--- sample/src/main/java/app/juky/squircleview/sample/MainActivity.kt
+++ sample/src/main/java/app/juky/squircleview/sample/MainActivity.kt
@@ -1,11 +1,14 @@
 package app.juky.squircleview.sample
 
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import app.juky.squircleview.sample.databinding.ActivityMainBinding
+import app.juky.squircleview.utils.SquircleShape
 import app.juky.squircleview.views.SquircleImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -24,6 +27,15 @@ class MainActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             loadImageWithResourceLoading()
         }, 5000)
+
+        // The methods down below are native ShapeDrawable and ShapeAppearance usages
+        binding.buttonWithShapeDrawable.shapeAppearanceModel = SquircleShape.getShapeAppearance().build()
+        binding.imageWithShapeAppearance.shapeAppearanceModel = SquircleShape.getShapeAppearance().build()
+        binding.constraintLayoutWithShapeDrawable.background = SquircleShape.getShapeDrawable(binding.constraintLayoutWithShapeDrawable).apply {
+            this.paint.apply {
+                this.color = ContextCompat.getColor(this@MainActivity, R.color.teal_700)
+            }
+        }
     }
 
     private fun loadImageDrawable() {
@@ -33,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadImageWithResourceLoading() {
-        Glide.with( applicationContext).load(R.drawable.second_image)
+        Glide.with(applicationContext).load(R.drawable.second_image)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(object : CustomViewTarget<SquircleImageView, Drawable>(binding.imageButton) {
                 override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
